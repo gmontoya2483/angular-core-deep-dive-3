c83b395a150f569463c23338769505b260e87c5a
Gabriel Montoya <gabi.montoya@gmail.com>
2023-11-27 17:34:19 -0300

    completed 09 Angula Injectable Service in Depth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
 import {HighlightedDirective} from './directives/highlighted.directive';
 import {Observable} from 'rxjs';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {CoursesService} from './services/courses.service';
 
 @Component({
   selector: 'app-root',
@@ -12,16 +14,21 @@ import {Observable} from 'rxjs';
 })
 export class AppComponent implements OnInit {
 
+  courses$: Observable<Course[]>;
+  courses;
 
-  courses = COURSES;
-
-  constructor() {
-
+  constructor(private courseService: CoursesService) {
   }
 
   ngOnInit() {
+    this.courses$ = this.courseService.loadCourses();
   }
 
 
+  save(course: Course) {
+    this.courseService.saveCourse(course).subscribe(
+      () => console.log('Course Saved!')
+    );
 
+  }
 }
