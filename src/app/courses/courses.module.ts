070990a0c8fad6d6e010497ce54d7f865bd85ff4
Gabriel Montoya <gabi.montoya@gmail.com>
2023-12-05 16:08:43 -0300

    Completed pipes in depth

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -5,6 +5,7 @@ import {CourseImageComponent} from './course-image/course-image.component';
 import {CoursesService} from './courses.service';
 import {HighlightedDirective} from './directives/highlighted.directive';
 import {NgxUnlessDirective} from './directives/ngx-unless.directive';
+import {FilterByCategoryPipe} from './filter-by-category.pipe';
 
 
 
@@ -13,7 +14,8 @@ import {NgxUnlessDirective} from './directives/ngx-unless.directive';
     CourseCardComponent,
     CourseImageComponent,
     HighlightedDirective,
-    NgxUnlessDirective
+    NgxUnlessDirective,
+    FilterByCategoryPipe
   ],
   imports: [
     CommonModule
@@ -21,6 +23,7 @@ import {NgxUnlessDirective} from './directives/ngx-unless.directive';
   exports: [
     CourseCardComponent,
     CourseImageComponent,
+    FilterByCategoryPipe,
   ],
   providers: [CoursesService]
 })
